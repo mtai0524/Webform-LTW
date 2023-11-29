b20afb74cb26f0f45783d53c134a61de3f864278
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2023-11-29 09:31:27 +0700

    test mailstrap.io

--- NonBaoHiemRoyalHelmet/NonBaoHiemRoyalHelmet/GioHang.aspx.cs
+++ NonBaoHiemRoyalHelmet/NonBaoHiemRoyalHelmet/GioHang.aspx.cs
@@ -159,7 +159,7 @@ namespace NonBaoHiemRoyalHelmet
             }
 
         }
-
+      
         void SendMail(DonHang donHang)
         {
             List<SanPham> cart = (List<SanPham>)Session["Cart"];
@@ -243,11 +243,11 @@ namespace NonBaoHiemRoyalHelmet
             mail.Body = body.ToString();
             mail.IsBodyHtml = true; // Bật chế độ HTML
 
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            SmtpClient smtp = new SmtpClient("sandbox.smtp.mailtrap.io");
             smtp.EnableSsl = true;
-            smtp.Port = 587;
+            smtp.Port = 2525;
             smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtp.Credentials = new NetworkCredential("duatreodaiduongden@gmail.com", "aiyt kzuj xpbq ygda");
+            smtp.Credentials = new NetworkCredential("f53ec0c5d129dd", "647d8437d3d40c");
 
             try
             {
@@ -259,6 +259,106 @@ namespace NonBaoHiemRoyalHelmet
                 Response.Write($"Lỗi khi gửi mail: {ex.Message}");
             }
         }
+
+        //void SendMail(DonHang donHang)
+        //{
+        //    List<SanPham> cart = (List<SanPham>)Session["Cart"];
+        //    StringBuilder body = new StringBuilder();
+        //    body.AppendLine("<html><head>");
+        //    body.AppendLine("<style>");
+        //    body.AppendLine("body { font-family: Arial, sans-serif; margin: 0; padding: 0; }");
+        //    body.AppendLine(".container { width: 80%; margin: auto; overflow: hidden; }");
+        //    body.AppendLine("header { background: #103151; color: white; padding-top: 30px; min-height: 70px; border-bottom: #D4BB72 4px solid; }");
+        //    body.AppendLine("header a { color: white; text-decoration: none; text-transform: uppercase; font-size: 16px; }");
+        //    body.AppendLine("header ul { padding: 0; margin: 0; float: right; margin-top: 20px; list-style: none; }");
+        //    body.AppendLine("header #branding { float: left; margin: 0; padding: 0; }");
+        //    body.AppendLine("header #branding img { height: 80px; }");
+        //    body.AppendLine("header .highlight, header .current a { color: #E8BD72; font-weight: bold; }");
+        //    body.AppendLine("header a:hover { color: #ffffff; font-weight: bold; }");
+        //    body.AppendLine(".main { padding: 0; }");
+        //    body.AppendLine(".main h2 { color: #333; }");
+        //    body.AppendLine(".main p { font-size: 18px; color: #666; }");
+        //    body.AppendLine("table { border-collapse: collapse; width: 100%; }");
+        //    body.AppendLine("th, td { border: 1px solid #dddddd; text-align: left; padding: 12px; font-size: 16px; }");
+        //    body.AppendLine("th { background-color: #f2f2f2; }");
+        //    body.AppendLine("</style>");
+        //    body.AppendLine("</head><body>");
+        //    body.AppendLine("<header>");
+        //    body.AppendLine("<div class=\"container\">");
+        //    body.AppendLine("<div id=\"branding\">");
+        //    // Thêm ảnh logo vào giữa header
+        //    body.AppendLine("<img src=\"https://royalhelmet.com.vn/ckfinder/userfiles/images/logo.png\" alt=\"Logo\" />");
+        //    body.AppendLine("<h1><span class=\"highlight\">Royal Helmet</span></h1>");
+        //    body.AppendLine("</div>");
+        //    body.AppendLine("</div>");
+        //    body.AppendLine("</header>");
+        //    body.AppendLine("<div class=\"container main\">");
+        //    body.AppendLine("<h2>Chi tiết đơn hàng:</h2>");
+
+        //    body.AppendLine($"<p><strong>Mã đơn hàng:</strong> {donHang.MaDH}</p>");
+        //    body.AppendLine($"<p><strong>Người đặt hàng:</strong> {Session["FullName"].ToString()}</p>");
+        //    body.AppendLine($"<p><strong>Số điện thoại:</strong> {Session["Phone"].ToString()}</p>");
+        //    body.AppendLine($"<p><strong>Email:</strong> {Session["Email"].ToString()}</p>");
+        //    body.AppendLine($"<p><strong>Địa chỉ:</strong> {Session["Address"].ToString()}</p>");
+
+        //    body.AppendLine($"<p><strong>Ngày đặt hàng:</strong> {donHang.NgayDH.ToString("dd/MM/yyyy HH:mm")}</p>");
+        //    body.AppendLine($"<p><strong>Sản phẩm:</strong></p>");
+
+
+        //    body.AppendLine("<table>");
+        //    body.AppendLine("<tr><th>Tên sản phẩm</th><th>Giá bán</th><th>Số lượng</th><th>Tổng giá</th></tr>");
+        //    double tongTien = 0;
+        //    List<CTDH> chiTietDonHang = context2.CTDHs.Include("SanPham")
+        //                                      .Where(ct => ct.MaDH == donHang.MaDH)
+        //                                      .ToList();
+        //    foreach (CTDH item in chiTietDonHang)
+        //    {
+        //        body.AppendLine($"<tr>");
+        //        body.AppendLine($"<td>{item.SanPham.TenSP}</td>");
+        //        body.AppendLine($"<td>{item.DonGia.ToString("N0")} ₫</td>");
+        //        body.AppendLine($"<td>{item.SoLuong}</td>");
+        //        double tongGia = item.DonGia * item.SoLuong;
+        //        body.AppendLine($"<td>{tongGia.ToString("N0")} ₫</td>");
+        //        body.AppendLine($"</tr>");
+        //        tongTien += item.DonGia * item.SoLuong;
+        //    }
+        //    body.AppendLine($"<tr><td colspan='3'><strong>Tổng tiền:</strong></td><td>{tongTien.ToString("N0")} ₫</td></tr>");
+        //    body.AppendLine("</table>");
+
+        //    body.AppendLine("</div>");
+
+        //    body.AppendLine("<footer style=\"margin-top:20px;background: #262626; color: white; padding: 20px 0;\">");
+        //    body.AppendLine("<div class=\"container\">");
+        //    body.AppendLine("<p>&copy; 2023 Royal Helmet. </p>");
+        //    body.AppendLine("</div>");
+        //    body.AppendLine("</footer>");
+
+        //    body.AppendLine("</body></html>");
+
+        //    // Gửi email
+        //    MailMessage mail = new MailMessage();
+        //    mail.To.Add(Session["Email"].ToString());
+        //    mail.From = new MailAddress("duatreodaiduongden@gmail.com");
+        //    mail.Subject = "ROYAL HELMET - CHI TIẾT ĐƠN HÀNG TEST";
+        //    mail.Body = body.ToString();
+        //    mail.IsBodyHtml = true; // Bật chế độ HTML
+
+        //    SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+        //    smtp.EnableSsl = true;
+        //    smtp.Port = 587;
+        //    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+        //    smtp.Credentials = new NetworkCredential("duatreodaiduongden@gmail.com", "aiyt kzuj xpbq ygda");
+
+        //    try
+        //    {
+        //        smtp.Send(mail);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // Xử lý nếu có lỗi khi gửi mail
+        //        Response.Write($"Lỗi khi gửi mail: {ex.Message}");
+        //    }
+        //}
 
 
 
